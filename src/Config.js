5c1a2c8940ce55e6198048cc32a4e3680f3ebb42
Franck De Girolami <franck@codeparticle.com>
2020-07-14 14:23:20 -0700

    Setting up our React app

---
+++ src/Config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-west-2",
+    BUCKET: "franck-notes-app-uploads"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://ysq3zjqsid.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_mz9bcthJ2",
+    APP_CLIENT_ID: "1cgnf2c4sckt6k8nr8pm9g5oi9",
+    IDENTITY_POOL_ID: "us-west-2:600e6c1e-0ab5-4f00-9263-47157242e4f0"
+  }
+};
