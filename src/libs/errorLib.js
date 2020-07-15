359fb5305a6c31d514a3600bc69247376f2a7fd3
Franck De Girolami <franck@codeparticle.com>
2020-07-15 15:09:29 -0700

    Adding error reporting

--- src/libs/errorLib.js
+++ src/libs/errorLib.js
@@ -1,10 +1,41 @@
+import * as Sentry from "@sentry/browser";
+
+const isLocal = process.env.NODE_ENV === "development";
+
+export function initSentry() {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.init({dsn: "https://822089b2f74a497c9f8592aa18a1bf4f@o420931.ingest.sentry.io/5339958"});
+}
+
+export function logError(error, errorInfo = null) {
+  if (isLocal) {
+    return;
+  }
+
+  Sentry.withScope((scope) => {
+    errorInfo && scope.setExtras(errorInfo);
+    Sentry.captureException(error);
+  });
+}
+
 export function onError(error) {
+  let errorInfo = {};
   let message = error.toString();
 
   // Auth errors
   if (!(error instanceof Error) && error.message) {
+    errorInfo = error;
     message = error.message;
+    error = new Error(message);
+    // API errors
+  } else if (error.config && error.config.url) {
+    errorInfo.url = error.config.url;
   }
+
+  logError(error, errorInfo);
 
   alert(message);
 }
